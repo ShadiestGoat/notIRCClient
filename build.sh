6107e87194e0d1bd7133e1eb2bad0a26f86dc95e
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2023-09-24 00:17:15 +0100

    Remove extra .

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ BASE_LOCATION=""
 # Keep this as 's', unless you're testing on a server without ssl
 SECURE_S="s"
 
-go build -ldflags "-s -w -X 'main.AUTHOR_NAME=$AUTHOR_NAME' -X 'main.AUTHOR_COLOR=$AUTHOR_COLOR' -X 'main.BASE_LOCATION=$BASE_LOCATION' -X 'main.SECURE_S=$SECURE_S'" .
+go build -ldflags "-s -w -X 'main.AUTHOR_NAME=$AUTHOR_NAME' -X 'main.AUTHOR_COLOR=$AUTHOR_COLOR' -X 'main.BASE_LOCATION=$BASE_LOCATION' -X 'main.SECURE_S=$SECURE_S'"
